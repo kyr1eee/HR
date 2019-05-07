3ab860ecf024beace8f4d360d25f1850f505ce6f
15mao <1005148749@qq.com>
2019-05-08 00:45:02 +0800

    公司信息接口

--- server/dbs/config.js
+++ server/dbs/config.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const config = mysql.createConnection({
-  host: 'localhost',
+  host: '47.106.247.211',
   port: 3306,
   user: 'root',
-  password: '123456',
-  database: 'stuer'
+  password: '781246808',
+  database: 'HR'
 })
 
 module.exports = config
--- server/interface/company.js
+++ server/interface/company.js
@@ -1,6 +1,6 @@
 import Router from 'koa-router'
 //import axios from './axios'
-const sql = require('../dbs/index')
+const sql = require('../dbs/config')
 
 let router = new Router({
   prefix: '/company'
@@ -9,12 +9,33 @@ let router = new Router({
 router.post('/submit', async (ctx) => {
   let params = ctx.request.body
   let res=await new Promise((resolve,reject)=>{
-    sql.query('insert into companies(name,info) values(?,?)',[params.name,params.desc],(error,result,filed)=>{
+    sql.query('insert into companies(name,regNum,address,business,property,people,info) values(?,?,?,?,?,?,?)',[params.name,params.regNum,params.address,params.business,params.property,params.people,params.info],(error,result,filed)=>{
         if (error) return error;
         resolve(result)
     })
   })
-  ctx.response.body={status:1,msg:'操作成功',data:res}
+  ctx.response.body={status:200,msg:'操作成功',data:res}
+})
+
+router.post('/save', async (ctx) => {
+  let params = ctx.request.body
+  let res=await new Promise((resolve,reject)=>{
+    sql.query('update companies(name,regNum,address,business,property,people,info) values(?,?,?,?,?,?,?) where id = 3',[params.name,params.regNum,params.address,params.business,params.property,params.people,params.info],(error,result,filed)=>{
+        if (error) return error;
+        resolve(result)
+    })
+  })
+  ctx.response.body={status:200,msg:'操作成功',data:res}
+})
+
+router.get('/infos', async (ctx) => {
+  let res=await new Promise((resolve,reject)=>{
+    sql.query('select * from posts where id = 3', (error,result,filed)=>{
+        if (error) return error;
+        resolve(result)
+    })
+  })
+  ctx.response.body={status:200,msg:'操作成功',data:res}
 })
 
 export default router
